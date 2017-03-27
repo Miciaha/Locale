c61133da73ebb6433b1b73024df4a4f5dd002c5c
Miciaha Ivey <mdi13b@my.fsu.edu>
2017-03-26 22:35:08 -0400

    Removed 'app' in 'db=SQLALchemy(app)'

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
 from routes import app
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = 'users'
