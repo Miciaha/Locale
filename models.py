2c007e545444772883314102a0c46ce677a87d44
Miciaha Ivey <mdi13b@my.fsu.edu>
2017-03-26 22:23:53 -0400

    imported 'app' to model

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import generate_password_hash, check_password_hash
+from routes import app
 
-db = SQLAlchemy()
+db = SQLAlchemy(app)
 
 class User(db.Model):
     __tablename__ = 'users'
