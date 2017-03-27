74176e7ef3566cb7f6ea923b96e18a4173aa7cd7
Miciaha Ivey <mdi13b@my.fsu.edu>
2017-03-27 07:20:27 -0400

    new home, aint no place like it

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from forms import SignupForm, LoginForm
+from forms import SignupForm, LoginForm, AddressForm
 from flask_sqlalchemy import SQLAlchemy
 import os
 
@@ -79,7 +79,17 @@ def home():
     if 'email' not in session:
         return redirect(url_for('login'))
 
-    return render_template("home.html")
+    form = AddressForm()
+
+    if request.method == 'POST':
+        if form.validate() == False:
+            return render_template('home.html', form=form)
+        else:
+            #handle form submission
+            pass
+
+    elif request.method == 'GET':
+        return render_template("home.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
